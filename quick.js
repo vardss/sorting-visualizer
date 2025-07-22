dd71f17acaaad73645566df03a51d5ac7876e45b
VardS <vardhithas@gmail.com>
2025-07-22 21:58:14 +0530

    Update quick.js

--- quick.js
+++ quick.js
@@ -1,37 +1,38 @@
 async function lomuto(ele,l,h){
     let i=l-1;
     
-    ele[h].style.background='#892CDC';
+    ele[h].style.background='red';
     for(let j=l; j<=h-1; j++)
     {
-        ele[j].style.background='#77B0AA';
+        ele[j].style.background='brown';
         await delay(time);
         if(parseInt(ele[j].style.height) < parseInt(ele[h].style.height))
         {
-            if(i>=l)
+              if(i>=l)
             {
-                ele[i].style.background='#BC6FF1';
+                ele[i].style.background='SlateBlue';
             }
+          
             i++;
             swap(ele[i],ele[j]);
-            ele[i].style.background='#EBD3F8';
-            // if(i!=j)
-            // {
-                // ele[j].style.background='orange';
-            // }
+            ele[i].style.background='orange';
             await delay(time);
         }
-        ele[j].style.background='#BC6FF1';
+        ele[j].style.background='SlateBlue';
     }
     if(i>=l){
-        ele[i].style.background='#BC6FF1';
+        ele[i].style.background='SlateBlue';
     }
 
     await delay(time);
     swap(ele[i+1],ele[h]);
     
-    ele[h].style.background='#BC6FF1';
-    
+    ele[h].style.background='SlateBlue';
+
+    await delay(time);
+    ele[i+1].style.background='SpringGreen'
+
+
     await delay(time);      
         return i+1;
     }
@@ -43,27 +44,27 @@ async function lomuto(ele,l,h){
         await qsort(ele,l,p-1);
         await qsort(ele,p+1,h);
     }
+
+    if(l==h)
+    {
+        ele[l].style.background='SpringGreen'
+    }
     
     else{
         return;
     }
 }
 
-// qsort(ele,l,h);
+
 document.getElementById('quick').addEventListener('click', async function(){
     let ele=document.querySelectorAll('.sort');
     let l=0;
     let h=parseInt(ele.length)-1;
     disableSizeSlider();
     disableSortingBtn();
-   document.getElementById("theo").innerHTML = "Quick sort has an average-case time complexity of O(n log n), and a worst-case time complexity of O(n<sup>2</sup>) when the pivot selection is poor. It is an in-place and not always stable algorithm, with a space complexity of O(log n).";
+   document.getElementById("explanation").innerHTML = "Quick sort has an average-case time complexity of O(n log n), and a worst-case time complexity of O(n<sup>2</sup>) when the pivot selection is poor. It is an in-place and not always stable algorithm, with recursive space complexity of O(log n).";
 
     await qsort(ele,l,h);
-    for(let i=0; i<=h; i++)
-    {
-        await delay(time);
-        ele[i].style.background='#52057B';
-    }
     enableSizeSlider();
     enableSortingBtn();
 });
