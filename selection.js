e50d86e3d69e6bc917a18ed5ac7a6a486286a7a6
VardS <vardhithas@gmail.com>
2025-07-22 21:59:09 +0530

    Update selection.js

--- selection.js
+++ selection.js
@@ -3,34 +3,38 @@ const ele=document.querySelectorAll('.sort');
 for(let i=0; i<ele.length; i++)
 {
     let x=i;
-    ele[i].style.background='#892CDC';
+    ele[i].style.background='red';
     for(let j=i+1; j<ele.length; j++)
     {
-        ele[j].style.background='#892CDC';
+        //ele[j].style.background='red';
         if(parseInt(ele[j].style.height)<parseInt(ele[x].style.height))
         {
-            if(x!==i)
-            {
-                ele[x].style.background='#BC6FF1';
-            }
-            x=j;
+           // if(x!==i)
+           // {
+               // ele[x].style.background='SlateBlue';
+          //  }
+           // x=j;
+           ele[x].style.background='SlateBlue';
+           x=j;
+           ele[j].style.background='red';
+
         }
 
-        else
-        {
-            ele[j].style.background='#BC6FF1';
-        }
+         //else
+         // {
+         //   ele[j].style.background='SlateBlue';
+         //}
     }
     await delay(time);
     swap(ele[i],ele[x]);
-    ele[x].style.background='#BC6FF1';
-    ele[i].style.background='#52057B';
+    ele[x].style.background='SlateBlue';
+    ele[i].style.background='SpringGreen';
 }
 }
 document.getElementById("selection").addEventListener('click', async function(){
     disableSizeSlider();
     disableSortingBtn();
-    document.getElementById("theo").innerHTML = "Selection sort has a time complexity of O(n<sup>2</sup>) in the best, average, and worst cases. It is an in-place but not a stable sorting algorithm, with a space complexity of O(1).";
+    document.getElementById("explanation").innerHTML = "Selection sort has a time complexity of O(n<sup>2</sup>) in the best, average, and worst cases. It is an in-place but not a stable sorting algorithm, with a space complexity of O(1).";
 
 
     await selection();
